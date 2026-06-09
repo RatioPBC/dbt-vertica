3645c7a0e69b32bd8aba09be45012c73e95ac3da
Jesse Cooke <jesse@ratiopbc.com>
2026-06-09 14:45:33 -0700

    Add new volatile catalog query with tests

--- dbt/include/vertica/macros/adapters/catalog.sql
+++ dbt/include/vertica/macros/adapters/catalog.sql
@@ -27,6 +27,86 @@
   
 
 
+{% macro vertica__get_catalog_volatile(information_schema, schemas) -%}
+    {% set query %}
+        with tables as (
+            select * from (
+                select
+                    tab.table_id as id,
+                    '{{ information_schema.database }}' table_database,
+                    tab.table_schema,
+                    tab.table_name,
+                    'TABLE' table_type,
+                    tab.owner_name table_owner,
+                    col.column_id,
+                    col.column_name,
+                    col.ordinal_position column_index,
+                    col.data_type column_type
+                from v_catalog.tables tab
+                join v_catalog.columns col on tab.table_id = col.table_id
+            ) t
+            {{ vertica__get_catalog_schemas_where_clause_sql(schemas) }}
+        ), views as (
+            select * from (
+                select
+                    vw.table_id as id,
+                    '{{ information_schema.database }}' table_database,
+                    vw.table_schema,
+                    vw.table_name,
+                    'VIEW' table_type,
+                    vw.owner_name table_owner,
+                    col.column_id,
+                    col.column_name,
+                    col.ordinal_position column_index,
+                    col.data_type column_type
+                from v_catalog.views vw
+                join v_catalog.view_columns col on vw.table_id = col.table_id
+            ) v
+            {{ vertica__get_catalog_schemas_where_clause_sql(schemas) }}
+        ), table_comments as (
+            select
+                oid as id,
+                objectoid as table_id,
+                comment
+            from v_internal.vs_comments_p com
+            where objectoid in (select id from tables)
+        ), view_comments as (
+            select
+                oid as id,
+                objectoid as view_id,
+                comment
+            from v_internal.vs_comments_p com
+            where objectoid in (select id from views)
+        ), tables_with_comments as (
+            select
+                t.*,
+                tc.comment as table_comment
+            from tables t
+            left join table_comments tc on t.id = tc.table_id
+        ), views_with_comments as (
+            select
+                v.*,
+                vc.comment as table_comment
+            from views v
+            left join view_comments vc on v.id = vc.view_id
+        ), tables_and_views as (
+            select * from tables_with_comments twc
+            union all
+            select * from views_with_comments vwc
+        )
+        select
+            tav.*,
+            com.comment as column_comment
+        from tables_and_views tav
+        left join v_internal.vs_sub_comments com
+            on com.objectoid = split_part(tav.column_id, '-', 1)::int
+            and com.childobject = tav.column_name
+        ;
+    {%- endset -%}
+    {{ return(run_query(query)) }}
+{%- endmacro %}
+
+
 {% macro vertica__get_catalog_relations(information_schema, relations) -%}
    {% set query %}
 
--- tests/functional/adapter/catalog/test_catalog_comments.py
+++ tests/functional/adapter/catalog/test_catalog_comments.py
@@ -59,3 +59,105 @@ class TestGetCatalogComments:
         assert view_rows[0]["table_type"] == "VIEW"
         assert view_rows[0]["table_comment"] == "view comment B"
         assert view_rows[0]["column_comment"] is None
+
+
+class TestGetCatalogVolatile:
+    @pytest.fixture(scope="class", autouse=True)
+    def setup_relations(self, project):
+        schema = project.test_schema
+        exec_sql(
+            project,
+            f"""
+          CREATE TABLE {schema}.cat_tbl (id int, name varchar(50));
+          COMMENT ON TABLE {schema}.cat_tbl IS 'table comment A';
+          COMMENT ON COLUMN {schema}.cat_tbl.id IS 'col comment id';
+          COMMENT ON COLUMN {schema}.cat_tbl.name IS 'col comment name';
+          CREATE VIEW {schema}.cat_vw AS SELECT id FROM {schema}.cat_tbl;
+          COMMENT ON VIEW {schema}.cat_vw IS 'view comment B';
+      """,
+        )
+        yield
+
+    def get_catalog(self, project):
+        info_schema = SimpleNamespace(database=project.database)
+        return run_macro(
+            project,
+            "vertica__get_catalog_volatile",
+            information_schema=info_schema,
+            schemas=[project.test_schema],
+        )
+
+    def test_result_columns(self, project):
+        table = self.get_catalog(project)
+        assert list(table.column_names) == [
+            "id",
+            "table_database",
+            "table_schema",
+            "table_name",
+            "table_type",
+            "table_owner",
+            "column_id",
+            "column_name",
+            "column_index",
+            "column_type",
+            "table_comment",
+            "column_comment",
+        ]
+
+    def test_result_values(self, project):
+        table = self.get_catalog(project)
+        owner = project.adapter.config.credentials.username
+        rows = {
+            (r["table_name"], r["column_name"]): dict(zip(table.column_names, r))
+            for r in table.rows
+        }
+        assert len(rows) == 3
+
+        tbl_id = rows[("cat_tbl", "id")]["id"]
+        vw_id = rows[("cat_vw", "id")]["id"]
+        assert tbl_id is not None
+        assert vw_id is not None
+        assert tbl_id != vw_id
+
+        assert rows[("cat_tbl", "id")] == {
+            "id": tbl_id,
+            "table_database": project.database,
+            "table_schema": project.test_schema,
+            "table_name": "cat_tbl",
+            "table_type": "TABLE",
+            "table_owner": owner,
+            "column_id": f"{tbl_id}-1",
+            "column_name": "id",
+            "column_index": 1,
+            "column_type": "int",
+            "table_comment": "table comment A",
+            "column_comment": "col comment id",
+        }
+        assert rows[("cat_tbl", "name")] == {
+            "id": tbl_id,
+            "table_database": project.database,
+            "table_schema": project.test_schema,
+            "table_name": "cat_tbl",
+            "table_type": "TABLE",
+            "table_owner": owner,
+            "column_id": f"{tbl_id}-2",
+            "column_name": "name",
+            "column_index": 2,
+            "column_type": "varchar(50)",
+            "table_comment": "table comment A",
+            "column_comment": "col comment name",
+        }
+        assert rows[("cat_vw", "id")] == {
+            "id": vw_id,
+            "table_database": project.database,
+            "table_schema": project.test_schema,
+            "table_name": "cat_vw",
+            "table_type": "VIEW",
+            "table_owner": owner,
+            "column_id": f"{vw_id}-1",
+            "column_name": "id",
+            "column_index": 1,
+            "column_type": "int",
+            "table_comment": "view comment B",
+            "column_comment": None,
+        }
